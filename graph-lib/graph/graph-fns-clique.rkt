2741793852de2c5ef775a340dc92536f719649ec
Ameya Dubey <ameyadubey1618@gmail.com>
2021-11-11 12:42:36 +0530

    Add comments for clique functions, minor refactoring

--- graph-lib/graph/graph-fns-clique.rkt
+++ graph-lib/graph/graph-fns-clique.rkt
@@ -26,6 +26,29 @@
     body ...
     (loop)))
 
+;; ----------------------------------------------------------------
+;; find-cliques
+;;
+;; Takes an undirected graph G and returns a generator
+;; which yields each maximal clique in the graph once.
+;; A clique is represented as a list of vertices.
+;; The algorithm terminates when the generator yields #<void>.
+;;
+;; NOTE: This algorithm is inspired by the Python NetworkX
+;; implementation of the algorithm in this paper.
+;;
+;;    Etsuji Tomita, Akira Tanaka, Haruhisa Takahashi,
+;;    "The worst-case time complexity for generating all maximal
+;;    cliques and computational experiments",
+;;    *Theoretical Computer Science*, Volume 363, Issue 1,
+;;    Computing and Combinatorics,
+;;    10th Annual International Conference on
+;;    Computing and Combinatorics (COCOON 2004),
+;;    25 October 2006, Pages 28--42
+;;    <https://doi.org/10.1016/j.tcs.2006.06.015>
+;;
+;; TODO: Make this implementation more idiomatic, if possible
+;;
 (define (find-cliques G)
   (define (set-argmax s f)
     (define init (set-first s))
@@ -34,20 +57,27 @@
                #:when (> (f v) max-val))
       (values (f v) v)))
   
+  ;; get-u takes two sets - subg and cand, and returns
+  ;; u = { v ∈ subg : | adj(v) ∩ cand | is maximized }
   (define (get-u subg cand)
     (set-argmax subg
-      (λ (v)
-        (set-count (set-intersect (list->set (get-neighbors G v)) cand)))))
+                (λ (v) (set-count (set-intersect (list->set (get-neighbors G v)) cand)))))
 
+  ;; get-ext-u takes a vertex u and a mutable-set cand,
+  ;; and returns a new mutable-set cand - adj(u)
   (define (get-ext-u u cand)
     (define adj-u (list->set (get-neighbors G u)))
     (for/mutable-set ([v (in-mutable-set cand)]
                       #:unless (set-member? adj-u v))
       v))
 
+  ;; returns a new mutable-set A ∩ B
   (define (set-intersect/mutable A B)
-    (for/mutable-set ([v (in-mutable-set A)]
-                      #:when (set-member? B v))
+    (define-values (intersectand intersector)
+      (if (<= (set-count A) (set-count B))
+          (values A B) (values B A)))
+    (for/mutable-set ([v (in-set intersectand)]
+                      #:when (set-member? intersector v))
       v))
 
   (generator ()
@@ -81,6 +111,14 @@
                [else (pop! Q)
                      (set!-values (subg cand ext-u) (apply values (pop! stack)))]))))))
 
+
+;; ----------------------------------------------------------------
+;; in-cliques & get-cliques
+;;
+;; in-cliques  : expands to a function call that returns a
+;;               sequence of maximal cliques
+;; get-cliques : returns a list of maximal cliques
+;;
 (define-syntax-rule (in-cliques G)
   (in-producer (find-cliques G) (void)))
 
